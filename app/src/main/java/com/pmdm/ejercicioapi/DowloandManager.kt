bdbffd6ac012b2af686146b655d65255807ba8b6
nuria <airun.lr92@gmail.com>
2021-02-09 14:19:58 +0100

    Fourth commit

--- app/src/main/java/com/pmdm/ejercicioapi/DowloandManager.kt
+++ app/src/main/java/com/pmdm/ejercicioapi/DowloandManager.kt
@@ -20,34 +20,40 @@ class DowloandManager {
             val request = Request.Builder()
                     .url(url)
                     .build()
-            val response = client.newCall(request).execute()
-            if(!response.isSuccessful){
-                println("Error en la llamada")
-            }
-            val responseHeaders = response.headers
+            val call = client.newCall(request)
+            val exc = call.execute()
 
             delay(3000)
-            return listOf(response.toString())
+            return listOf(exc.body.toString())
 
         }
 
         suspend fun downloadApiSingleResult(userChoice: String): String {
             val client = OkHttpClient()
-            val url = "https://www.anapioficeandfire.com/api/books"
+            var url = "https://www.anapioficeandfire.com/api/books"
+            when (userChoice) {
+                "1" -> url+="/1"
+                "2" -> url+="/2"
+                "3" -> url+="/3"
+                "4" -> url+="/4"
+                "5" -> url+="/5"
+                "6" -> url+="/6"
+                "7" -> url+="/7"
+                "8" -> url+="/8"
+                "9" -> url+="/9"
+                "10"-> url+="/10"
+                "11" -> url+="/11"
+                "12" -> url+="/12"
+                else -> "No hay ningún libro con ese id. Prueba otro inferior"
+            }
             val request = Request.Builder()
                     .url(url)
                     .build()
-            val response = client.newCall(request).execute()
-            if(!response.isSuccessful){
-                println("Error en la llamada")
-            }
-            val responseHeaders = response.headers
+            val call = client.newCall(request)
+            val exc = call.execute()
+
             delay(3000)
-            return when (userChoice) {
-                "1" -> "1"
-                "2" -> "2"
-                else -> "OTROS RESULTADOS"
-            }
+            return exc.body.toString()
 
         }
     }
